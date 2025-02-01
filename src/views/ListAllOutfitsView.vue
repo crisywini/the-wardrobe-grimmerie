7f5e244cac67ac2e467398e47d2d3e29ec3ae4f5
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2025-02-01 13:58:53 -0500

    Add animations and functions to select categories and outfits

--- src/views/ListAllOutfitsView.vue
+++ src/views/ListAllOutfitsView.vue
@@ -4,32 +4,34 @@
     <div class="box" />
 
     <div class="category-container">
-      <h1>Gym wear</h1>
-      <h1>Casual</h1>
-      <h1>Streetwear</h1>
-
+      <h1 v-for="category in categories" :key="category" class="category" @click="selectCategory(category)"> {{ category
+        }}</h1>
     </div>
-
 
     <div class="outfits-container">
-
-      <img src="/public/images/o1.png">
-      <img src="/public/images/o2.png">
-      <img src="/public/images/o3.png">
-      <img src="/public/images/o4.png">
-      <img src="/public/images/o1.png">
-      <img src="/public/images/o2.png">
-      <img src="/public/images/o3.png">
-      <img src="/public/images/o4.png">
-
+      <img v-for="path in images" :key="path" :src="path" class="outfit" @click="selectOutfit(path)">
     </div>
-
-
-
-
 
   </div>
 </template>
+
+<script setup lang="ts">
+
+const images = ["/public/images/o1.png", "/public/images/o2.png", "/public/images/o3.png", "/public/images/o4.png"]
+
+const categories = ["gym wear", "casual", "streetwear"]
+
+const selectOutfit = (path: string) => {
+  console.log(path);
+}
+
+const selectCategory = (category: string) => {
+  console.log('Category Selected', category)
+}
+
+
+</script>
+
 
 <style scoped>
 .parent-container {
@@ -54,16 +56,30 @@
 }
 
 .outfits-container {
-
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(250px, 0fr));
-  gap: 1px;
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
 }
 
-.outfits-container img {
+.outfit {
   width: 100%;
   height: auto;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  transition: transform 0.3s, box-shadow 0.3s;
+}
 
+.outfit:hover {
+  transform: scale(1.1);
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);
+  cursor: pointer;
+}
+
+.category {
+  transition: transform 0.3s, box-shadow 0.3s;
+}
+
+.category:hover {
+  transform: scale(1.1);
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);
+  cursor: pointer;
 }
 </style>
